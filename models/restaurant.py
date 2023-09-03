349f2d816f59f38982ce15413071d153828320c1
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-04 00:21:49 +0300

    Mission Passed

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, func
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -11,11 +11,16 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    price = Column(Integer)
 
     reviews = relationship("Reviews", back_populates="restaurant")
 
-    def __init__(self, name):
-        self.name = name  
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def fanciest(cls, session):
+        return session.query(cls).order_by(cls.price.desc()).first()  
 
     def average_star_rating(self, session):
         reviews = session.query(Review).filter_by(restaurant=self).all()
@@ -28,8 +33,15 @@ class Restaurant(Base):
         return average_rating
     
     def get_reviews(self, session):
-        return session.query(Review).filter_by(restaurant = self).all()
-    
+        reviews = session.query(Review).filter_by(restaurant = self).all()
+        formatted_reviews = [
+            f"Review for {self.name} by {review.customer.get_full_name()}: {review.rating} stars."
+            for review in reviews
+        ]
+        return formatted_reviews
+
+
+
     def get_customers(self, session):
         reviews = session.query(Review).filter_by(restaurant = self).all()
         customers = set([review.customer for review in reviews])
