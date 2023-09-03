016890577d2b5618ce7e2005e88a5285e960dd03
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-03 18:15:14 +0300

    Mission Passed

--- models/restaurant.py
+++ models/restaurant.py
@@ -1,65 +1,42 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+from models.customer import Customer  # Import Customer class
 
 Base = declarative_base()
 
 class Restaurant(Base):
     __tablename__ = 'restaurants'
 
-
     id = Column(Integer, primary_key=True)
     name = Column(String)
 
-    reviews = relationship("Reviews", back_populates="restaurant")
+    reviews = relationship("Review", back_populates="restaurant")
 
     def __init__(self, name):
-        self.name = str(name)
+        self.name = name  # No need to convert to str
 
-    def get_name(self):
-        return self.name
-    
-    def get_customers(self):
-        session = Session()
-        customers = [review.customer for review in session.query(Reviews).filter_by(restaurant=self).all()]
-        session.close()
-        return list(set(customers))
-    
-    def get_reviews(self):
-        session  = Session()
-        reviews = session.query(Reviews).filter_by(restaurant=self).all()
-        session.close()
-        return reviews
-    
-    def average_star_rating(self):
-        session = Session()
-        reviews = session.query(Reviews).filter_by(restaurant=self).all()
-        session.close()
+    def average_star_rating(self, session):
+        reviews = session.query(Review).filter_by(restaurant=self).all()
 
         if not reviews:
-            return 0
-        
+            return None
+
         total_rating = sum(review.rating for review in reviews)
         average_rating = total_rating / len(reviews)
         return average_rating
-        
 
-
-class Reviews(Base):
+class Review(Base):
     __tablename__ = 'reviews'
-    
 
     id = Column(Integer, primary_key=True)
-    customer_id = Column(Integer, ForeignKey('customers.id'))
+    customer_id = Column(Integer, ForeignKey('customer.id'))
     restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
     rating = Column(Integer)
 
     customer = relationship("Customer", back_populates="reviews")
     restaurant = relationship("Restaurant", back_populates="reviews")
-
 
     def __init__(self, customer, restaurant, rating):
         self.customer = customer
@@ -69,3 +46,5 @@ class Reviews(Base):
 engine = create_engine('sqlite:///reviews.db')
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
+
+# You can now use the Session instance to manage your database sessions.
